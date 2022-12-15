71a8bb621ae5de6f9964f072a48861b3a3ea90c3
Li <mail@e.lixingyong.com>
2022-12-15 16:15:19 +0800

    :tada: 初次提交，确定部分技术栈

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import "./styles/main.css";
+export function count(x: number, y: number) {
+  return x + y;
+}
---
+++ templates/assets/dist/main.iife.js
@@ -0,0 +1,1 @@
+var main=function(n){"use strict";const e="";function t(u,c){return u+c}return n.count=t,Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),n}({});
